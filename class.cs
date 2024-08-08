2b0ea30854fd51105b920360ea1bf1c72a9bc322
glacami <161377388+glamica@users.noreply.github.com>
2024-08-08 18:32:14 +0000

    "nonononono"

--- class.cs
+++ class.cs
@@ -3,21 +3,32 @@
 
 using System;
 public class MySqlInterface {
-    private object MySQL
+    private object MySQL;
     public MySqlInterface(
         string host = "localhost",
         int port = 3306,
         string usuario = "root",
         string senha = "")
     {
-        Console.WriteLine($"MySQL.Socket(host <- '{host}'; port <- {port}; user <- '{user}'; password <- '{senha}')")
-        MySQL = new object(){Host=host,Port=port,User=usuario,Password=senha}
+        Console.WriteLine($"MySQL.Socket(host <- '{host}'; port <- {port}; user <- '{usuario}'; password <- '{senha}')");
+        MySQL = new {Host=host,Port=port,User=usuario,Password=senha};
+    }
+    public List<List<string>> Query(string query) {
+        Console.WriteLine($"query executada: {query}");
+        List<List<string>> resultSet = new List<List<string>>();
+        List<string> e = new List<string>();
+        for (int c = 0;c <= 90; c++) {
+            for (int r = 0; r <= 12; r++) e.Add($"string({c}) -> {r}");
+            resultSet.Add(e);
+            resultSet.Clear();
+        }
+        return e;
     }
 }
 public class Work
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine ("Try programiz.pro");
+        MySqlInterface e = new MySqlInterface();
     }
 }
